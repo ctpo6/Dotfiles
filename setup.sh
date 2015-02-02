45aa06a6227f579d3631b4e722c25e8896188a4f
Yuri Epstein <yuri.epstein@yaliny.com>
2015-02-02 16:53:26 +0300

    tmux

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+if [ -f ~/.tmux.conf ]; then
+	rm -f ~/.tmux.conf
+fi
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
 
 if [ -f ~/.bash_aliases ]; then
 	cp -f ~/.bash_aliases ~/.bash_aliases.old
 fi
+ln -sf $(pwd)/bash_aliases ~/.bash_aliases
 
 if [ -f ~/.bash_aliases ]; then
 	cp -f ~/.bashrc ~/.bashrc.old
 fi
-
-ln -sf $(pwd)/bash_aliases ~/.bash_aliases
 ln -sf $(pwd)/bashrc ~/.bashrc

a7835a9937c02e1366b788b5bb66d33b98d8004a
Yuri Epstein <yuri.epstein@yaliny.com>
2015-02-02 17:02:25 +0300

    tmuxinator

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -f ~/.tmux.conf ]; then
-	rm -f ~/.tmux.conf
-fi
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
-
 if [ -f ~/.bash_aliases ]; then
 	cp -f ~/.bash_aliases ~/.bash_aliases.old
 fi
@@ -14,3 +9,13 @@ if [ -f ~/.bash_aliases ]; then
 	cp -f ~/.bashrc ~/.bashrc.old
 fi
 ln -sf $(pwd)/bashrc ~/.bashrc
+
+if [ -f ~/.tmux.conf ]; then
+	rm -f ~/.tmux.conf
+fi
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
+
+if [ -d ~/.tmuxinator ]; then
+	rm -rf ~/.tmuxinator
+fi
+ln -s $(pwd)/tmuxinator ~/.tmuxinator
